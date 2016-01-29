2e6e167690ed8570569f671a47225dbe0183dd91
Victor SALLARD <sallardvictor@gmail.com>
2016-01-29 14:11:21 +0100

    Retablissement

---
+++ DataAccessLayer/DataAccess.cs
@@ -0,0 +1,32 @@
+﻿using JediTournamentEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    interface DataAccess
+    {
+        Jedi GetJedi(int Id);
+        Match GetMatch(int Id);
+        Stade GetStade(int Id);
+
+        List<Jedi> GetJedis();
+        List<Match> GetMatchs();
+        List<Stade> GetStades();
+
+        void AddJedi(Jedi jedi);
+        void AddMatch(Match match);
+        void AddStade(Stade stade);
+
+        void UpdateJedi(Jedi jedi);
+        void UpdateMatch(Match match);
+        void UpdateStade(Stade stade);
+
+        void DeleteJedi(Jedi jedi);
+        void DeleteMatch(Match match);
+        void DeleteStade(Stade stade);
+    }
+}
