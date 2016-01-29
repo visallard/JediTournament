caf70aef162ab04e80a6cc043cc1c66eb8ed4f96
JulienUsson <usson.julien@gmail.com>
2016-01-29 14:52:49 +0100

    Fenetre Jedi

---
+++ JediTournamentWPF/View/Jedis.xaml.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer;
+using JediTournamentEntities;
+using JediTournamentWPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JediTournamentWPF.View
+{
+    /// <summary>
+    /// Logique d'interaction pour Jedis.xaml
+    /// </summary>
+    public partial class Jedis : Window
+    {
+        public Jedis()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // récupération des artistes
+            JediTournamentManager jm = new JediTournamentManager();
+            List<Jedi> jedis = jm.GetJedis();
+
+            // Initialisation du viewModel
+            JedisModelView jmv = new JedisModelView(jedis);
+            jedisListView.DataContext = jmv;
+        }
+    }
+}
---
+++ JediTournamentWPF/ViewModel/JedisModelView.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer;
+using JediTournamentEntities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JediTournamentWPF.ViewModel
+{
+    class JedisModelView : ViewModelBase
+    {
+        private ObservableCollection<Jedi> _jedis;
+        public ObservableCollection<Jedi> Jedis
+        {
+            get { return _jedis; }
+            private set
+            {
+                _jedis = value;
+                OnPropertyChanged("Jedis");
+            }
+        }
+
+        public JedisModelView(List<Jedi> jedisModel)
+        {
+            _jedis = new ObservableCollection<Jedi>();
+            foreach (Jedi j in jedisModel)
+            {
+                _jedis.Add(j);
+            }
+        }
+    }
+}
