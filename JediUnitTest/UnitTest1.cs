bca0474a8c2701c7a8a2c02ebfb674e0e57fe3e3
Victor SALLARD <sallardvictor@gmail.com>
2016-02-04 17:56:19 +0100

    One Unit Test;

--- JediUnitTest/UnitTest1.cs
+++ JediUnitTest/UnitTest1.cs
@@ -12,11 +12,11 @@ namespace DataAccessLayer.Tests
         public void AddJediTest()
         {
             BusinessLayer.JediTournamentManager test = new BusinessLayer.JediTournamentManager();
-            JediTournamentEntities.Jedi insert = new JediTournamentEntities.Jedi("JarJar", true);
+            JediTournamentEntities.Jedi insert = new JediTournamentEntities.Jedi("Palpatine", true);
             test.AddJedi(insert);
             List<JediTournamentEntities.Jedi> jedis = test.GetJedis().ToList<JediTournamentEntities.Jedi>();
 
-            Assert.IsTrue(jedis.Contains(insert));
+            Assert.IsTrue(jedis.Exists(x => x.Nom== "Palpatine"));
         }
     }
 }
