6787e3514e2d837bc2c465ab3e2fe1becc88108c
Victor SALLARD <sallardvictor@gmail.com>
2016-02-05 14:08:23 +0100

    Fixed TM;

--- BusinessLayer/JediTournamentManager.cs
+++ BusinessLayer/JediTournamentManager.cs
@@ -72,40 +72,9 @@ namespace BusinessLayer
         }
 
         //public IEnumerable<Tournoi> GetTournois()
-        public ObservableCollection<Tournoi> GetTournois() // Quand tu ajoute la BBD pour les tournois décommente tes lignes et change mes "ObservableCollection" par le bon type stp
+        public IEnumerable<Tournoi> GetTournois() // Quand tu ajoute la BBD pour les tournois décommente tes lignes et change mes "ObservableCollection" par le bon type stp
         {
-            // Données pour tester le binding
-            Stade stade1 = new Stade(1000, "Tatooine");
-            Stade stade2 = new Stade(5000, "Mustafar");
-
-            Jedi jedi1 = new Jedi("Paul", false);
-            Jedi jedi2 = new Jedi("Julien", false);
-            Jedi jedi3 = new Jedi("Antoine", true);
-            Jedi jedi4 = new Jedi("Victor", true);
-            Jedi jedi5 = new Jedi("Elian", true);
-
-            List<Match> matchs1 = new List<Match>();
-            matchs1.Add(new Match(0, jedi1, jedi1, jedi2, EPhaseTournoi.DemiFinale, stade1));
-            matchs1.Add(new Match(1, jedi4, jedi3, jedi4, EPhaseTournoi.DemiFinale, stade2));
-            matchs1.Add(new Match(2, jedi1, jedi1, jedi4, EPhaseTournoi.Finale, stade1));
-
-            List<Match> matchs2 = new List<Match>();
-            matchs2.Add(new Match(0, jedi1, jedi1, jedi3, EPhaseTournoi.DemiFinale, stade2));
-            matchs2.Add(new Match(1, jedi2, jedi2, jedi4, EPhaseTournoi.DemiFinale, stade1));
-            matchs2.Add(new Match(2, jedi1, jedi1, jedi2, EPhaseTournoi.Finale, stade2));
-
-            List<Match> matchs3 = new List<Match>();
-            matchs3.Add(new Match(0, jedi1, jedi1, jedi4, EPhaseTournoi.DemiFinale, stade2));
-            matchs3.Add(new Match(1, jedi3, jedi2, jedi3, EPhaseTournoi.DemiFinale, stade1));
-            matchs3.Add(new Match(2, jedi1, jedi1, jedi3, EPhaseTournoi.Finale, stade2));
-            
-            ObservableCollection<Tournoi> tournois = new ObservableCollection<Tournoi>();
-            tournois.Add(new Tournoi(0, "TournoiDeTest0", matchs1));
-            tournois.Add(new Tournoi(1, "TournoiDeTest1", matchs2));
-            tournois.Add(new Tournoi(2, "TournoiDeTest2", matchs3));
-
-            //return DalManager.Instance.GetTournois();
-            return tournois;
+            return DalManager.Instance.GetTournois();
         }
     }
 }
