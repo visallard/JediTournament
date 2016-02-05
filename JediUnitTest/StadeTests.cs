0d6fe7c17da846bc0dfd4e7a952fe7afdbab2806
Victor SALLARD <sallardvictor@gmail.com>
2016-02-05 14:24:33 +0100

    Moar tests;

---
+++ JediUnitTest/StadeTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLayer;
+using JediTournamentEntities;
+using System.Linq;
+
+namespace StadeTests
+{
+    [TestClass]
+    public class StadeTests
+    {
+        [TestMethod]
+        public void AddStadeTest()
+        {
+            JediTournamentManager jtm = new JediTournamentManager();
+            Stade stade = new Stade(12, "Naboo");
+            jtm.AddStade(stade);
+            var stades = jtm.GetStades();
+            Assert.IsTrue(stades.Last().Equals(stade));
+        }
+
+        [TestMethod()]
+        public void DelStadeTest()
+        {
+            JediTournamentManager jtm = new JediTournamentManager();
+            var stades = jtm.GetStades();
+            jtm.DelStade(stades.Last());
+            Assert.IsTrue(stades.Count() - 1 == jtm.GetStades().Count());
+        }
+    }
+}
