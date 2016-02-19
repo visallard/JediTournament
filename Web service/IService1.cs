4b15763b403e5204d4f2cfcb73a3a2f375b7bdae
Anne-lise Michel <michel_anne-lise@orange.fr>
2016-02-19 15:40:49 +0100

    Added WebService & more ;

---
+++ Web service/IService1.cs
@@ -0,0 +1,68 @@
+﻿using JediTournamentEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using BusinessLayer;
+
+namespace Web_service
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IService1
+    {
+    
+        [OperationContract]
+        IEnumerable<Jedi> GetJedi();
+        [OperationContract]
+        IEnumerable<Match> GetMatch();
+        [OperationContract]
+        IEnumerable<Stade> GetStade();
+        [OperationContract]
+        IEnumerable<Tournoi> GetTournoi();
+
+
+        // TODO: ajoutez vos opérations de service ici
+    }
+
+
+    // Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service.
+    [DataContract]
+    public class CompositeType
+    {
+        //JediTournamentManager manager;
+
+        IEnumerable<Jedi> jedis;
+        IEnumerable<Match> matchs;
+        IEnumerable<Stade> stades;
+        IEnumerable<Tournoi> tournois;
+
+        [DataMember]
+        public IEnumerable<Jedi> Jedis
+        {
+            get { return jedis; }
+            set { jedis = value; }
+        }
+
+        public IEnumerable<Match> Matchs
+        {
+            get { return matchs; }
+            set { matchs = value; }
+        }
+
+        public IEnumerable<Stade> Stades
+        {
+            get { return stades; }
+            set { stades = value; }
+        }
+
+        public IEnumerable<Tournoi> Tournois
+        {
+            get { return tournois; }
+            set { tournois = value; }
+        }
+    }
+}
---
+++ Web service/Service1.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using JediTournamentEntities;
+
+namespace Web_service
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class Service1 : IService1
+    {
+        public IEnumerable<Jedi> GetJedi()
+        {
+            BusinessLayer.JediTournamentManager m = new BusinessLayer.JediTournamentManager();
+            return null;
+        }
+
+        public IEnumerable<Match> GetMatch()
+        {
+            BusinessLayer.JediTournamentManager m = new BusinessLayer.JediTournamentManager();
+            return m.GetMatchs();
+        }
+
+        public IEnumerable<Tournoi> GetTournoi()
+        {
+            BusinessLayer.JediTournamentManager m = new BusinessLayer.JediTournamentManager();
+            return m.GetTournois();
+        }
+
+        public IEnumerable<Stade> GetStade()
+        {
+            BusinessLayer.JediTournamentManager m = new BusinessLayer.JediTournamentManager();
+            return m.GetStades();
+        }
+    }
+}
